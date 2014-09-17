4e24dc2cabfb146428cff4233294d6c8c63eb956
fifaquake <lipengweb@gmail.com>
2014-09-17 21:00:47 +0800

    Fixi the issues caused by removing realgames.ejs

--- routes/games.js
+++ routes/games.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('realGames', { title: 'Welcome to real games!' });
+    res.render('games', { title: 'Welcome to real games!' });
 });
 
 module.exports = router;
