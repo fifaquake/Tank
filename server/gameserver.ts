f35520e739ded23115bf01a02f24b17e38103c73
fifaquake.home <lipengweb@gmail.com>
2014-09-21 17:29:32 +0800

    Introduce TypeScript to Tank

---
+++ server/gameserver.ts
@@ -0,0 +1,21 @@
+/// <reference path="../d.ts.files/node.d.ts" />
+exports.startGameServer = function(expressServer) {
+	var clientCount = 0;
+	var io = require('socket.io')(expressServer);
+	io.on('connection', (socket) => {
+		clientCount++;
+		console.log('a user connected');
+		socket.on('disconnect', function(){
+			clientCount--;
+			console.log('user disconnected');
+		});
+
+		var resource;
+		if (clientCount === 1) {
+			resource = 'p1tank';
+		} else {
+			resource = 'p2tank';
+		}
+		socket.emit('welcome', {'resource' : resource});
+	});
+};
