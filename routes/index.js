0a400d6676bcd786a716b8b22b111111aeee2684
Kentaxer <i_wangyuyue@hotmail.com>
2014-12-17 23:22:16 +0800

    improve page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome to real games!' });
 });
 
 module.exports = router;
